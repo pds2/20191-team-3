cdc8775064e319e7cfeef83b8e98414d9ac46cc6
GuilhermeAquim <48455074+GuilhermeAquim@users.noreply.github.com>
2019-05-21 20:00:27 -0300

    Add files via upload

--- healingitem.cpp
+++ healingitem.cpp
@@ -2,14 +2,14 @@
 #include "healingitem.h"
 using namespace std;
 
-HealingItem::HealingItem(string nome)
+HealingItem::HealingItem(string nome)//Construtor
 {
-	if(nome=="Potion")
+	if(nome=="Potion")//Caso o HealingItem seja Potion
 	{
 		_nome=nome;
 		_HealPts=10;
 	}
-	else if(nome=="Elixir")
+	else if(nome=="Elixir")//Caso o HealingItem seja Elixir
 	{
 		_nome=nome;
 		_HealPts=20;
@@ -18,9 +18,9 @@ HealingItem::HealingItem(string nome)
 
 void HealingItem::exibir_status()
 {
-	if(_nome=="Potion")
+	if(_nome=="Potion")//Mensagem caso Potion
 		return "Use este item para recuperar 10 pontos de HP";
-	else if(_nome=="Elixir")
+	else if(_nome=="Elixir")//Mensagem caso Elixir
 		return "Use este item para recuperar 20 pontos de HP";
 }
 
