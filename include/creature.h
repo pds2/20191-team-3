83ab39394988f32bafb096778ab49a7ab12910dd
Vitor Ribeiro <vit.riber1@hotmail.com>
2019-05-25 13:30:02 -0300

    Create creature.h

---
+++ include/creature.h
@@ -0,0 +1,23 @@
+#include "iostream"
+
+using namespace std;
+
+/*Esta classe inicializa os três atributos: ataque, defesa e bater - pontos. 
+Em seguida, ela usa um algoritmo de ataque para determinar se o ataque foi bem sucedido , 
+o dano do ataque e os restantes pontos atingidos . */
+
+
+class CCreature
+{
+    private:
+        unsigned int muiAttack;
+        unsigned int muiDefense;
+        unsigned int muiHitPoints;
+    public:
+        CCreature();
+        //Função que calcula o valor de ataque e se foi bem sucedido
+        void Ataque(CCreature &qrDefender);
+        //Função que retorna se o heroi morreu
+        bool IsDead();
+}
+;
