25dc0b4d4138275cbcb8430159b96a2790b0164c
GuilhermeAquim <48455074+GuilhermeAquim@users.noreply.github.com>
2019-05-27 10:35:16 -0300

    Add files via upload

--- src/arma.cpp
+++ src/arma.cpp
@@ -2,13 +2,48 @@
 #include "arma.h"
 using namespace std;
 
+//Construtor paramêtros resgatados de um txt
+Arma::Arma(string nome, string tipo, int att, int hit, int crit, int rmin, int rmax):
+_nome(nome), _tipo(tipo), _attack(att), _hit(hit), _crit(crit), _range_min(rmin), _range_max(rmax){}
+
+//retorna string com todos os dados concatenados
 void Arma::exibir_status()
 {
-	return _nome;//Nome da Arma
-	return _tipo;//Tipo da Arma (espada, machado, lança, arco, tomo)
-	return _attack;//Pontos de ataque da arma
-  	return _hit;//Pontos de hit (chance de acertar)
-  	return _crit;//pontos de crítico (chance do ataque desconsiderar defesa) 
-  	return _range_min;//alcance mínimo da arma
-  	return _range_max;//alcance máximo da arma
+	
+}
+
+//Métodos Get
+string Arma::get_nome()
+{
+	return _nome;
+}
+
+string Arma::get_tipo()
+{
+	return _tipo;
+}
+
+int Arma::get_attack()
+{
+	return _attack;
+}
+
+int Arma::get_hit()
+{
+	return _hit;
+}
+
+int Arma::get_crit()
+{
+	return _crit;
+}
+
+int Arma::get_range_min()
+{
+	return _range_min;
+}
+
+int Arma::get_range_max()
+{
+	return _range_max;
 }
