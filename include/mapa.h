3ff0fa581e99e008218dfa5952241b61ec8f1aa1
Vitor Ribeiro <vit.riber1@hotmail.com>
2019-05-29 20:06:17 -0300

    Update mapa.h

--- include/mapa.h
+++ include/mapa.h
@@ -1,12 +1,22 @@
+#include <iostream>
+
 //Crie uma nova classe que representa o seu " calabouço " como uma matriz bi- dimensional 
 
 
 class CDungeon
 {
     private:
-        char mcaaMaze[10][10];
+        char mcaaMaze[10][12] = {"******* ***",
+                                "******" , 
+                                "*****" , 
+                                "**" , 
+                                "******" , 
+                                "*** ****",
+                                "****",
+                                "****",
+                                "******",
+                                "**********"};
     public:
         CDungeon();
         char GetMazeSquare(unsigned int uiRow, unsigned int uiCol);
-}
-;
+};
