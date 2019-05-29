1226cb90e7069c144b7e7c7255688b0986f2c4a6
Vitor Ribeiro <vit.riber1@hotmail.com>
2019-05-29 20:04:10 -0300

    Update mapa.cpp

--- src/mapa.cpp
+++ src/mapa.cpp
@@ -1,20 +1,18 @@
+#include "mapa.h"
+
 //Crie uma nova classe que representa o seu " calabouço " como uma matriz bi- dimensional 
 
 
-class CDungeon
-{
-    public:
-        CDungeon()
-        { //Criar uma mazechar vazio caaMaze 
-        [ 10] [11 ] = {" ******* *** " , " ****** " , " ***** " , " ** " , " ****** " , " *** **** "," **** "," **** "," ****** "," ********** " }; 
-        for (unsigned int uiRow = 0; uiRow < 10; + +uiRow) {
-            for (unsigned int uiCol = 0; uiCol < 10; + +uiCol) {
-                mcaaMaze[uiRow][uiCol] = caaMaze[uiRow][uiCol];
-            }
+CDungeon::CDungeon(){
+{ //Criar uma mazechar vazio caaMaze 
+    for (unsigned int uiRow = 0; uiRow < 10; ++uiRow) {
+        for (unsigned int uiCol = 0; uiCol < 10; ++uiCol) {
+            mcaaMaze[uiRow][uiCol] = mcaaMaze[uiRow][uiCol];
         }
-        }
-        caractere GetMazeSquare(unsigned int uiRow, unsigned int uiCol) { return mcaaMaze[uiRow][uiCol]; }
-    private:
-        char mcaaMaze[10][10];
+    }
 }
-;
+
+
+char CDungeon::GetMazeSquare(unsigned int uiRow, unsigned int uiCol){ 
+    return mcaaMaze[uiRow][uiCol]; 
+}
