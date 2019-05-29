e92a189eb53cadbec99f7d52c53ba16754049b11
Vinícius Alexandre <48128959+vasilva@users.noreply.github.com>
2019-05-29 10:29:19 -0300

    Add files via upload

--- include/mercenary.h
+++ include/mercenary.h
@@ -16,15 +16,23 @@ class Mercenary : public Personagem
     int _armaequipada;
     vector<Arma> _iventario_arma; //Max 3 Armas
   public:
+    //Construtor
     Mercenary(string nome, int HP, int strength, int skill, int speed, int luck, int defense, int resistence);
-    void Add_Arma(Arma arma);
-    void Mudar_Equip(Arma arma);
-
+    
     //Calculo dos parametros de batalha
 	int Atk();
 	int Hit();
 	int Crit();
 	int Avo();
+
+    //Conjunto de métodos get para os atributos
+    string get_Classe();
+    int get_Move();
+    
+    //Metodos do Iventario de Armas
+    void Add_Arma(Arma arma);
+    void Mudar_Equip(Arma arma);
+
 };
 
 #endif
